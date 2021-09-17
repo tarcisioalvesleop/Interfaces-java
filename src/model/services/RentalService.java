a34acb25ca376e9b995f6d181b5a577847bfc7a3
Tarcisio Alves Bertolino <tarcisioalvesleop@hotmail.com>
2021-09-17 15:43:29 -0300

    Calculo Invoice sem Interface.

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,6 +1,7 @@
 package model.services;
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalService {
 	private Double pricePerDay;
@@ -16,6 +17,21 @@ public class RentalService {
 	}
 	
 	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime(); //pegando o valor em milisegundos
+		long t2 = carRental.getFinish().getTime();//tempo final em milisegundos
+		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour; //pegando o valor de horas arendondado para cima
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment); //calcula o valor de imposto apartir do basicPayment
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));// cria um novo obj de Invoice e associa a car.Rental
 		
 	}
 	
